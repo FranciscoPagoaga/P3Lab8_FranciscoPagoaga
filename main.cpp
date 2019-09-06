827f1bfdb29fe840f915e92d3ee133d07e50d948
FranciscoPagoaga <50030247+FranciscoPagoaga@users.noreply.github.com>
2019-09-06 13:54:56 -0600

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+sqlite3 *conn;
+sqlite3_stmt *res;
+int error=0;
+int rec_count =0;
+const char *tail;
+
 
 int main(int argc, char** argv) {
+	bool cond=true;
+	while(cond){
+		cout<<"1) Agregar"<<endl
+		<<"2) Eliminar"<<endl
+		<<"3) Listar jefes"<<endl
+		<<"4) "
+		int opcion;
+		cin>>opcion;
+		switch(opcion){
+			case 1:{
+				cout<<"1) Departamentos"<<endl
+				<<"2) Empleados"<<endl;
+				int opcion2;
+				cin>>opcion2;
+				switch(opcion2){
+					case 1:{
+						cout<<"Ingrese numero del nuevo departamento"<<endl;
+						int numdepto;
+						cin>>numdepto;
+						cout<<"Ingrese nombre del nuevo departamento"<<endl;
+						string nombre;
+						cin>>nombre;
+						cout<<"Ingrese la ubicacion del departamento"<<endl;
+						
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}	
 	return 0;
 }
+
+
